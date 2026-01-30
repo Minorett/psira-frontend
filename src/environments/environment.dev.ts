f8ebc1e64747cb8d5cf8ecc9c01b35205f6344dd
Manuel <86536808+omnimu@users.noreply.github.com>
2026-01-30 15:09:56 -0300

    Update development environment settings
    Refactor environment configuration for development.

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,2 +1,9 @@
 import { EnvironmentBase } from './environment.base';
-export const environment = EnvironmentBase;
+/*export const environment = EnvironmentBase;*/
+export const environment = {
+  ...EnvironmentBase,
+  production: false,              // opcional, EnvironmentBase ya lo pone
+  hmr: false,                     // como lo tengas
+  email: false,                   // como lo tengas
+  baseURL: 'https://psira.localhost:8443/graphql',
+};
