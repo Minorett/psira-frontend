9a59942c294f9c4b5ee5d331d804637f9628f058
Minorett <125107191+Minorett@users.noreply.github.com>
2026-03-25 21:02:47 -0300

    Add authentication token to Apollo link

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 import { environment } from '../environments/environment';
 
 const uri = environment.baseURL;
 
 export function createApollo(httpLink: HttpLink) {
+  const auth = setContext(() => {
+    const tokenStr = localStorage.getItem('auth_app_token');
+    if (!tokenStr) return {};
+    try {
+      const { accessToken } = JSON.parse(tokenStr);
+      return accessToken
+        ? { headers: { Authorization: `Bearer ${accessToken}` } }
+        : {};
+    } catch {
+      return {};
+    }
+  });
+
   return {
-    link: httpLink.create({ uri }),
+    link: ApolloLink.from([auth, httpLink.create({ uri })]),
     cache: new InMemoryCache(),
   };
 }
